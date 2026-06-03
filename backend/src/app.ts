677218aa3f1b7c82fb1639312c02928e4d46e7bc
Priyabrata Das <daspriyabrat10@gmail.com>
2026-06-03 22:31:17 +0530

    feat: add originalKey field to Video model and implement S3 file upload endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { prisma } from "./lib/prisma";
-
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { s3 } from "./storage/s3";
 const app = express();
 
 app.use(cors());
@@ -20,4 +21,25 @@ app.get("/db-test", async (_, res) => {
 
   res.json(video);
 });
+
+app.get("/s3-test", async (_, res) => {
+  try {
+    await s3.send(
+      new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET_NAME!,
+        Key: "test.txt",
+        Body: "Hello from VideoFlow",
+      })
+    );
+
+    res.json({
+      success: true,
+      message: "File uploaded successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
 export default app;
