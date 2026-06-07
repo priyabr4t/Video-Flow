2c37f7a112ff011e00a38cf3cdf5f5da067beb30
Priyabrata Das <daspriyabrat10@gmail.com>
2026-06-07 09:32:53 +0530

    feat: implement video processing worker using BullMQ

---
+++ backend/src/worker/video.worker.ts
@@ -0,0 +1,12 @@
+import { Worker } from "bullmq";
+import { connection } from "../queue/connection"
+
+const worker = new Worker(
+    "video-processing",
+    async (job) => {
+        console.log("processing-video", job.data.videoId)
+    },
+    {
+        connection
+    }
+)
