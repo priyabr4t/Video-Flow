cc86cf4f1a47f81fc3b400f7e6232f66028720ec
Priyabrata Das <daspriyabrat10@gmail.com>
2026-06-26 00:46:02 +0530

    feat: add getCourseByIdHandler route to fetch course details

--- backend/src/routes/course.routes.ts
+++ backend/src/routes/course.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createCourseHandler, getCoursesHandler } from "../controllers/course.controllers";
+import { createCourseHandler, getCourseByIdHandler, getCoursesHandler } from "../controllers/course.controllers";
 import { requireAuth, requireRole } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/", requireAuth, requireRole("INSTRUCTOR"), createCourseHandler);
 router.get("/", requireAuth, requireRole("INSTRUCTOR", "STUDENT"), getCoursesHandler);
+router.get("/:courseId", requireAuth, requireRole("INSTRUCTOR", "STUDENT"), getCourseByIdHandler);
 
 export default router;
