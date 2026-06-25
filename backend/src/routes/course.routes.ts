dc0505110f23bb7893dea39fd1485b1b3b56dedd
Priyabrata Das <daspriyabrat10@gmail.com>
2026-06-26 02:14:54 +0530

    feat: add route to create lessons for a specific course

--- backend/src/routes/course.routes.ts
+++ backend/src/routes/course.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createCourseHandler, getCourseByIdHandler, getCoursesHandler } from "../controllers/course.controllers";
 import { requireAuth, requireRole } from "../middlewares/auth.middleware";
+import { createLessonHandler } from "../controllers/lesson.controllers";
 
 const router = Router();
 
 router.post("/", requireAuth, requireRole("INSTRUCTOR"), createCourseHandler);
 router.get("/", requireAuth, requireRole("INSTRUCTOR", "STUDENT"), getCoursesHandler);
 router.get("/:courseId", requireAuth, requireRole("INSTRUCTOR", "STUDENT"), getCourseByIdHandler);
+router.post("/:courseId/lessons", requireAuth, requireRole("INSTRUCTOR", "ADMIN"), createLessonHandler);
 
 export default router;
